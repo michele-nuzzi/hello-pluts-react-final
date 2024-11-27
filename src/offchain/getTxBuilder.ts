4b424aae3dff978585f2ebdb8362a98849baffc9
EehMauro <maurojoaquinescudero@gmail.com>
2024-11-27 08:20:36 -0300

    Updated plu-ts version

--- src/offchain/getTxBuilder.ts
+++ src/offchain/getTxBuilder.ts
@@ -1,4 +1,4 @@
-import { TxBuilder, defaultProtocolParameters } from "@harmoniclabs/plu-ts";
+import { TxBuilder, defaultProtocolParameters, toCostModelArrV3 } from "@harmoniclabs/plu-ts";
 import { BlockfrostPluts } from "@harmoniclabs/blockfrost-pluts";
 
 /**
@@ -11,7 +11,21 @@ let _cachedTxBuilder: TxBuilder | undefined = undefined
 export default async function getTxBuilder(Blockfrost: BlockfrostPluts): Promise<TxBuilder> {
   if (!(_cachedTxBuilder instanceof TxBuilder)) {
     const parameters = await Blockfrost.epochsLatestParameters();
+
+    (window as any).a = parameters.costModels.PlutusScriptV3;
+    (window as any).b = parameters; 
+    (window as any).e = (parameters as any).cost_models_raw.PlutusV3;
+    (window as any).f = toCostModelArrV3(
+      parameters.costModels.PlutusScriptV3!
+    );
+
     _cachedTxBuilder = new TxBuilder(parameters);
+
+    (window as any).c = _cachedTxBuilder.protocolParamters.costModels.PlutusScriptV3;
+    (window as any).d = toCostModelArrV3(
+      _cachedTxBuilder.protocolParamters.costModels.PlutusScriptV3!
+    );
+
   }
   return _cachedTxBuilder;
 }
